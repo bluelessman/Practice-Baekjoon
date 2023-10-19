13edc271132382a27f8a96f613be0a89da6d478b
백진우 <148052120+bluelessman@users.noreply.github.com>
2023-10-19 16:40:11 +0900

    [Bronze I] Title: 소인수분해, Time: 48 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,7 +1,8 @@
 N = int(input())
+
+for i in range(2,int(N**0.5)+2):
+    while N%i == 0:
+        print(i)
+        N //= i
 if N != 1:
-    for i in range(2,N+1):
-        while N%i == 0:
-          print(i)
-          N /= i
-            
+    print(N)
