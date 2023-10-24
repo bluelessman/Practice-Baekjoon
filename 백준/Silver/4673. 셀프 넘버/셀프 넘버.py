0e008491a0c9a14960e79cca05ff86bb45eb7f1f
백진우 <148052120+bluelessman@users.noreply.github.com>
2023-10-24 11:22:31 +0900

    [Silver V] Title: 셀프 넘버, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,10 +1,11 @@
-numlist = []
+numlist = [0 for _ in range(10001)]
 for i in range(10001):
   temp = 0
   for j in str(i):
     temp += int(j)
-  numlist.append(temp+i)
-for i in range(10001):
-  if i not in numlist:
+  if temp+i < 10001:
+    numlist[temp+i] = 1
+for i in range(1,10001):
+  if numlist[i] == 0:
     print(i)
         
