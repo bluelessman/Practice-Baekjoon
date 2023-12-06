4cf4705c35b5c481fe7636e4a4fbc376dd99ac1d
백진우 <148052120+bluelessman@users.noreply.github.com>
2023-12-06 14:39:37 +0900

    [Silver III] Title: 피보나치 함수, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,8 +1,11 @@
-import sys
-flist = [[1,0],[0,1]]
-n = int(sys.stdin.readline())
+fibList = [[1,0],[0,1]]
+def fib(n):
+    if len(fibList)<=n:
+        temp = fib(n-1)+fib(n-2)
+        fibList.append([temp[0]+temp[2],temp[1]+temp[3]])
+    return fibList[n]
+n = int(input())
 for i in range(n):
-  k = int(sys.stdin.readline())
-  for j in range(len(flist),k+1):
-    flist.append([flist[j-1][0]+flist[j-2][0],flist[j-1][1]+flist[j-2][1]])
-  print(flist[k][0],flist[k][1])
+    m = int(input())
+    ans = fib(m)
+    print(ans[0],ans[1])
