d0dac746be3a7d35708e2d2cfce53631ccf9a572
백진우 <148052120+bluelessman@users.noreply.github.com>
2023-12-12 10:16:13 +0900

    [Silver II] Title: N과 M (12), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,9 @@
+from itertools import combinations_with_replacement
+n,m=map(int,input().split())
+nlist = list(set(map(int,input().split())))
+nlist.sort()
+ans = list(combinations_with_replacement(nlist,m))
+for i in ans:
+  for j in i:
+    print(j,end=" ")
+  print()
